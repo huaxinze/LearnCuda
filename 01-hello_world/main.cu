68f8acfc245e15514eb1cec73b3f8df4d070a931
huaxinze <huaxinze@qq.com>
2024-06-02 16:16:20 +0800

    first version

---
+++ 01-hello_world/main.cu
@@ -0,0 +1,54 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <corecrt_wstdio.h>
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void helloworld()
+{
+    int i = threadIdx.x;
+    printf("HELLO WORLD from %d thread!\n", i);
+}
+
+int main()
+{
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        return 1;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    helloworld<<<1, 10>>>();
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        return 1;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        return 1;
+    }
+
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
